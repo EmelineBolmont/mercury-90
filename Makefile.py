cc75f273d6b7191dad8b7f7bd84b73ad7bdd5ab1
cossou <cossou@460c6e2a-1132-2aa5-ec39-c273a4c8f634>
2011-06-07 09:49:22 +0000

    Creation d'un Makefile.py qui permet de compiler les 3 executables
    git-svn-id: https://mercury-90.googlecode.com/svn@5 460c6e2a-1132-2aa5-ec39-c273a4c8f634

---
+++ Makefile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Script that is a Makefile-like, but in python, and for fortran90
+
+from make import *
+
+# We clean undesirable files. Indeed, we will compile everything everytime.
+clean(["o", "mod"])
+
+sources_filename = lister("*.f90")
+
+# We create the binaries
+make_binaries(sources_filename, ["mercury.f90", "element.f90", "close.f90"])
