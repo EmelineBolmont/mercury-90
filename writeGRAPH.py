d629ed065132d4c8fb5c206dc915b5a6577828a8
autiwa <autiwa@460c6e2a-1132-2aa5-ec39-c273a4c8f634>
2011-07-22 17:22:27 +0000

    writeGRAPH.py : correction mineure
    git-svn-id: https://mercury-90.googlecode.com/svn@86 460c6e2a-1132-2aa5-ec39-c273a4c8f634

--- writeGRAPH.py
+++ writeGRAPH.py
@@ -4,8 +4,8 @@
 # Of course, everything is not tested, but it is planed to test as many things as possible
 
 __author__ = "Christophe Cossou <cossou@obs.u-bordeaux1.fr>"
-__date__ = "1st july 2011"
-__version__ = "$Revision: 2.6 $"
+__date__ = "22 Juillet 2011"
+__version__ = "$Revision: 2.6.1 $"
 __credits__ = """We run a test simulation and erase all the files created after the tests. The simulations files are thought to be 
 in a "simu_test" subdirectory of the directory were are the sources (and binaries) of mercury (and this script)"""
 
@@ -17,9 +17,9 @@ objects = []
 for source in sources_filename:
 	objects.append(sourceFile(source))
 
-mercury = sourceFile.findSource["mercury"]
-element = sourceFile.findSource["element"]
-close = sourceFile.findSource["close"]
+mercury = sourceFile.findSource["mercury.f90"]
+element = sourceFile.findSource["element.f90"]
+close = sourceFile.findSource["close.f90"]
 
 excluded = ["types_numeriques", "physical_constant", "mercury_constant"]
 
