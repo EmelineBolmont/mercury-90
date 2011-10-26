b2c9809b4b28681076f96a46056f0c325421394e
ccossou@gmail.com <ccossou@gmail.com@460c6e2a-1132-2aa5-ec39-c273a4c8f634>
2011-10-26 16:08:15 +0000

    NEW VERSION : viscous dissipation of the disk, test ok for dissipation, boundary condition can be specified by a parameter ans other things.
    git-svn-id: https://mercury-90.googlecode.com/svn@148 460c6e2a-1132-2aa5-ec39-c273a4c8f634

--- mercury_constant.f90
+++ mercury_constant.f90
@@ -20,7 +20,7 @@ integer, parameter :: CMAX = 50 ! CMAX  = maximum number of close-encounter mini
 integer, parameter :: NMESS = 200 ! NMESS = maximum number of messages in message.in
 integer, parameter :: NFILES = 50 ! NFILES = maximum number of files that can be open at the same time
 real(double_precision), parameter :: HUGE = 9.9d29 ! HUGE  = an implausibly large number
-real(double_precision), parameter :: TINY=4.D-15 ! A small number
+real(double_precision), parameter :: TINY = 4.D-15 ! A small number
 
 !...   convergence criteria for danby
 real(double_precision), parameter :: DANBYAC= 1.0d-14
