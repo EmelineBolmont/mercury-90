0a70d7d6638fb5a0846ff3e14b92476e8d376a59
Christophe Cossou <ccossou@gmail.com>
2012-10-23 11:39:22 +0200

    adding options into Makefile.py

--- Makefile.py
+++ Makefile.py
@@ -6,6 +6,43 @@ from make import *
 
 import git_infos
 
+# Parameters
+debug = False
+gdb = False
+profiling = False
+
+isProblem = False
+problem_message = "The script can take various arguments :" + "\n" + \
+"(no spaces between the key and the values, only separated by '=')" + "\n" + \
+" * help : display a little help message on HOW to use various options" + "\n" + \
+" * debug : [%s] activate debug options" % debug + "\n" + \
+" * gdb : [%s] activate options for gdb" % gdb + "\n" + \
+" * profiling : [%s] activate options for profiling" % profiling + "\n" + \
+" Example : " + "\n" + \
+" Makefile.py gdb"
+
+# We get arguments from the script
+for arg in sys.argv[1:]:
+  try:
+    (key, value) = arg.split("=")
+  except:
+    key = arg
+  if (key == 'debug'):
+    debug = True
+  elif (key == 'gdb'):
+    gdb = True
+  elif (key == 'profiling'):
+    profiling = True
+  elif (key == 'help'):
+    isProblem = True
+  else:
+    print("the key '"+key+"' does not match")
+    isProblem = True
+
+if isProblem:
+  print(problem_message)
+  exit()
+
 git_infos.write_infos_in_f90_file()
 
 isModifs = git_infos.is_non_committed_modifs()
@@ -25,7 +62,7 @@ sourceFile.setCompilingOptions("-O3 -march=native -pipe -finit-real=nan")
 sources_filename = lister("*.f90")
 
 # We create the binaries
-make_binaries(sources_filename, ["mercury.f90", "element.f90", "close.f90"], debug=False, gdb=False, profiling=False)
+make_binaries(sources_filename, ["mercury.f90", "element.f90", "close.f90"], debug=debug, gdb=gdb, profiling=profiling)
 #~ make_binaries(sources_filename, {"mercury.f90":"mercury2", "element.f90":"element2", "close.f90":"close2"}, debug=False, gdb=False, profiling=False)
 
 if (isModifs):
