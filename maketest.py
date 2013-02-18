c9d3f10de38fbcda791f58cde2317b6336256b48
Autiwa <autiwa@gmail.com>
2013-02-18 23:48:18 +0100

    maketest: A new version of make.py is now released that only compile what is needed. It does not compile again the files whose source code has not been modified. A new option has been introduced in maketest.py to force the compilation of everything, especially when we change the branch

--- maketest.py
+++ maketest.py
@@ -11,26 +11,42 @@ import sys # to be able to retrieve arguments of the script
 ###############################################
 
 filename = "test_disk.f90"
+force = False # To force the compilation of every module
 
 isProblem = False
 problem_message = "Given in parameters is the name of the source code you want to compile." + "\n" + \
 "The default filename is : "+filename + "\n" + \
-"For instance :" + "\n" + \
+"The script can take various arguments :" + "\n" + \
+"(no spaces between the key and the values, only separated by '=')" + "\n" + \
+" * force : To force the compilation of every module even those not modified" + "\n" + \
+" * help : display a little help message on HOW to use various options" + "\n" + \
+" * name : (default='%s')The filename of the sourcecode you want to compile" % filename + "\n" + \
+"Example :" + "\n" + \
 "maketest.py resultant_torque.f90"
 
 # We get arguments from the script
 for arg in sys.argv[1:]:
-	if (arg == 'help'):
-		isProblem = True
-	else:
-		filename = arg
+  try:
+    (key, value) = arg.split("=")
+  except:
+    key = arg
+  if (key == 'help'):
+    isProblem = True
+  elif (key == 'force'):
+    force = True
+  elif (key == 'name'):
+    filename = value
+  else:
+    print("the key '"+key+"' does not match")
+    isProblem = True
 
 if isProblem:
-	print(problem_message)
-	exit()
+  print(problem_message)
+  exit()
 
 # We clean undesirable files. Indeed, we will compile everything everytime.
-clean(["o", "mod"])
+if force:
+  clean(["o", "mod"])
 
 sourceFile.setCompilator("gfortran")
 
