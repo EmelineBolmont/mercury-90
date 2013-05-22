1149733f444e4fb06f7e330f59f3a8b40a1bf25a
Emeline Bolmont <emeline@arguin>
2013-05-22 17:28:32 +0200

    I was writing tidal properties even if their was no tides

--- tides_constant_GR.f90
+++ tides_constant_GR.f90
@@ -198,23 +198,22 @@ subroutine write_simus_properties()
   write(10,*) ''
   if (tides.eq.1) then
      write(10,'(a,i1)') 'Number of planets tidally evolving =',ntid
+     do j = 1, ntid
+        write(10,'(a,i1)') 'PLANET',j
+        if ((jupiter(j-1).eq.0).or.(jupiter(j-1).eq.1)) then
+           write(10,'(a,f5.2,a,f5.2)') 'k2p =',k2p_terr,', rg2p =',rg2p_terr
+           write(10,'(a,f5.2,a,f5.2)') 'k2pdeltap =',k2pdeltap_terr,' day, dissplan =',dissplan(j)     
+        endif
+        if (jupiter(j-1).eq.2) then
+           write(10,'(a,f5.2,a,f5.2)') 'k2p =',k2p_gg,', rg2p =',rg2p_gg
+           write(10,'(a,f5.2,a,f5.2)') 'k2pdeltap =',k2pdeltap_gg,' day, dissplan =',dissplan(j)   
+        endif
+        if (jupiter(j-1).eq.3) then
+           write(10,'(a,f5.2,a,f5.2)') 'k2p =',k2p_what,', rg2p =',rg2p_what
+           write(10,'(a,f5.2,a,f5.2)') 'k2pdeltap =',k2pdeltap_what,' day, dissplan =',dissplan(j)
+        endif
+     enddo
   endif
-  do j = 1, ntid
-     write(10,'(a,i1)') 'PLANET',j
-     if ((jupiter(j-1).eq.0).or.(jupiter(j-1).eq.1)) then
-        write(10,'(a,f5.2,a,f5.2)') 'k2p =',k2p_terr,', rg2p =',rg2p_terr
-        write(10,'(a,f5.2,a,f5.2)') 'k2pdeltap =',k2pdeltap_terr,' day, dissplan =',dissplan(j)     
-     endif
-     if (jupiter(j-1).eq.2) then
-        write(10,'(a,f5.2,a,f5.2)') 'k2p =',k2p_gg,', rg2p =',rg2p_gg
-        write(10,'(a,f5.2,a,f5.2)') 'k2pdeltap =',k2pdeltap_gg,' day, dissplan =',dissplan(j)   
-     endif
-     if (jupiter(j-1).eq.3) then
-        write(10,'(a,f5.2,a,f5.2)') 'k2p =',k2p_what,', rg2p =',rg2p_what
-        write(10,'(a,f5.2,a,f5.2)') 'k2pdeltap =',k2pdeltap_what,' day, dissplan =',dissplan(j)
-     endif
-     
-  enddo
   write(10,*) ''
   close(10)
   
