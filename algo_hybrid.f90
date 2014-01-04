a39b49acc844e16319256e54e786a6948d71721e
Autiwa <autiwa@gmail.com>
2014-01-04 18:30:26 +0100

    mco_h2dh intent reshaped

--- algo_hybrid.f90
+++ algo_hybrid.f90
@@ -443,10 +443,19 @@ subroutine mco_h2dh (time,jcen,nbod,nbig,h,m,xh,vh,x,v,ngf,ngflag)
 
   
   ! Input/Output
-  integer,intent(in) :: nbod,nbig,ngflag
-  real(double_precision),intent(in) :: time,jcen(3),h,m(nbod),xh(3,nbod),vh(3,nbod),ngf(4,nbod)
+  integer, intent(in) :: nbod
+  integer, intent(in) :: nbig
+  integer, intent(in) :: ngflag
+  real(double_precision),intent(in) :: time
+  real(double_precision),intent(in) :: jcen(3)
+  real(double_precision),intent(in) :: h
+  real(double_precision),intent(in) :: m(nbod)
+  real(double_precision),intent(in) :: xh(3,nbod)
+  real(double_precision),intent(in) :: vh(3,nbod)
+  real(double_precision),intent(in) :: ngf(4,nbod)
   
-  real(double_precision),intent(out) :: v(3,nbod),x(3,nbod)
+  real(double_precision),intent(out) :: v(3,nbod)
+  real(double_precision),intent(out) :: x(3,nbod)
   
   ! Local
   integer :: j
